d830ef0d069e76a1d18a98e61e6fc3e2b7ae6818
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2025-10-01 23:28:18 -0400

    route checkout test

--- src/ui/products/product-list.tsx
+++ src/ui/products/product-list.tsx
@@ -10,49 +10,45 @@ export const ProductList = async ({ products }: { products: Commerce.MappedProdu
 
 	return (
 		<>
-			<ul className="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-				{products.map((product, idx) => (
-					<li key={product.id} className="group">
-						<YnsLink href={`/product/${product.metadata.slug}`}>
-							<article>
-								{product.images[0] && (
-									<div className="aspect-square w-full overflow-hidden bg-neutral-100 rounded-lg">
-										<Image
-											className="
-												w-full h-full object-cover object-center 
-												transition-transform duration-500 
-												group-hover:scale-105 group-hover:rotate-1 group-hover:opacity-80
-											"
-											src={product.images[0]}
-											width={768}
-											height={768}
-											loading={idx < 3 ? "eager" : "lazy"}
-											priority={idx < 3}
-											sizes="(max-width: 1024px) 100vw, (max-width: 1280px) 50vw, 700px"
-											alt={product.name}
-										/>
+			<ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+				{products.map((product, idx) => {
+					return (
+						<li key={product.id} className="group">
+							<YnsLink href={`/product/${product.metadata.slug}`}>
+								<article className="overflow-hidden bg-white">
+									{product.images[0] && (
+										<div className="rounded-lg aspect-square w-full overflow-hidden bg-neutral-100">
+											<Image
+												className="group-hover:rotate hover-perspective w-full bg-neutral-100 object-cover object-center transition-opacity group-hover:opacity-75"
+												src={product.images[0]}
+												width={768}
+												height={768}
+												loading={idx < 3 ? "eager" : "lazy"}
+												priority={idx < 3}
+												sizes="(max-width: 1024x) 100vw, (max-width: 1280px) 50vw, 700px"
+												alt=""
+											/>
+										</div>
+									)}
+									<div className="p-2">
+										<h2 className="text-xl font-medium text-neutral-700">{product.name}</h2>
+										<footer className="text-base font-normal text-neutral-900">
+											{product.default_price.unit_amount && (
+												<p>
+													{formatMoney({
+														amount: product.default_price.unit_amount,
+														currency: product.default_price.currency,
+														locale,
+													})}
+												</p>
+											)}
+										</footer>
 									</div>
-								)}
-
-								{/* Flat text under the image */}
-								<div className="mt-2">
-									<h2 className="text-lg font-semibold text-neutral-800">{product.name}</h2>
-									<footer className="text-base font-medium text-neutral-900">
-										{product.default_price.unit_amount && (
-											<p>
-												{formatMoney({
-													amount: product.default_price.unit_amount,
-													currency: product.default_price.currency,
-													locale,
-												})}
-											</p>
-										)}
-									</footer>
-								</div>
-							</article>
-						</YnsLink>
-					</li>
-				))}
+								</article>
+							</YnsLink>
+						</li>
+					);
+				})}
 			</ul>
 			<JsonLd jsonLd={mappedProductsToJsonLd(products)} />
 		</>
